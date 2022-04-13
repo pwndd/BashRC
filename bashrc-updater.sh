c0fdb5ceaf2b61809e85df4da34571c3a0e9ef33
pwndd <103446011+pwndd@users.noreply.github.com>
2022-04-13 19:05:45 +0300

    Add files via upload

--- bashrc-updater.sh
+++ bashrc-updater.sh
@@ -1,14 +1,20 @@
+RED='\033[1;31m'
 BLUE='\033[1;34m'
-clear #Starting with a clean slate
-echo -e "${BLUE}Welcome to the bashRC updating utility! ${NC}" #Intro Text, To welcome you guys!
+GREEN='\033[1;32m'
+NC='\033[0m'
+clear
+echo -e "${BLUE}Welcome to the bashRC updating utility! ${NC}"
 sleep "1"
-echo -e "${BLUE}Starting in 5 seconds! ${NC}" #Intro Text, To welcome you guys!
+echo -e "${BLUE}Starting in 5 seconds! ${NC}"
 sleep "5"
-echo "Make sure to update your system and install neofetch, starship, nano and git"
-echo "If these are not installed, there will be trouble"
-echo "This script is made for bash, and your current shell is "$SHELL", if you think there will be any issue, please quit the script."
-sleep "2"
-curl -fsSL https://raw.githubusercontent.com/pwndd/BashRC/main/bashrc.txt > ~/.bashrc
+clear
+sudo pacman -S neofetch nano starship git curl
+sudo pacman -Syy
+sudo pacman -Syu
+cd $HOME && curl -fsSL https://raw.githubusercontent.com/pwndd/BashRC/main/bashrc.txt > .bashrc
 source ~/.bashrc
-echo "Done"
-echo "Script made with advice from Jonte (https://jontes.page)
+neofetch
+echo -e "${RED}Thanks a lot Jonte (https://jontes.page) for making this script work! ${NC)"
+echo -e "${GREEN}Done! Rebooting in 5 seconds! ${NC}"
+sleep "5"
+reboot
